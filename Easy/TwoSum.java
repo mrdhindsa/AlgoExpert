538d65d084a7df70d2e7dccf110ccaf87a64c66b
Manpreet Dhindsa <46641374+mrdhindsa@users.noreply.github.com>
2019-10-02 16:52:52 -0700

    Update TwoSum.java

--- Easy/TwoSum.java
+++ Easy/TwoSum.java
@@ -3,10 +3,8 @@ public static int[] twoNumberSum(int[] array, int targetSum){
   ArrayList<Integer> a_list = new ArrayList<Integer>();
   for(int i = 0; i < array.length; i++){
     int curr = array[i];
-    if(a_list.contains(targetSum-curr)){
-      if(targetSum-curr > curr) return new int[]{curr, targetSum-curr};
-      else{ return new int[]{targetSum-curr,curr}; }
-    } 
+    if(a_list.contains(targetSum-curr) && targetSum-curr > curr) return new int[]{curr, targetSum-curr};
+    else if(a_list.contains(targetSum-curr) && targetSum-curr <= curr) return new int[]{targetSum-curr,curr};
     a_list.add(curr);
   }
   return new int[]{};
