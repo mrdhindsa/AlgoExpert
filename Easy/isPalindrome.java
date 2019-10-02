a657bc205f3209b560f376331fe0d0b79f5d3fad
Manpreet Dhindsa <46641374+mrdhindsa@users.noreply.github.com>
2019-10-02 13:05:30 -0700

    Update isPalindrome.java

--- Easy/isPalindrome.java
+++ Easy/isPalindrome.java
@@ -1,1 +1,9 @@
-hello
+public static boolean isPalindrome(String str){
+  int left = 0, right = str.length()-1;
+  while(left < right){
+    if(str.charAt(left) != str.charAt(right)) return false;
+    left++;
+    right--;
+  }
+  return true;
+}
