a7a9231f5f6b31a938a9612ff82f4d4343ab1fb5
Manpreet Dhindsa <46641374+mrdhindsa@users.noreply.github.com>
2019-10-04 11:52:45 -0700

    Create BST_traversal.java

---
+++ Medium/BST_traversal.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+static class BST {
+  public int value;
+  public BST left;
+  public BST right;
+
+  public BST(int value) {
+    this.value = value;
+  }
+}
+
+// Using Recursion //
+public static ArrayList<Integer> inOrderTraverse(BST tree, ArrayList<Integer> array) {
+  if(tree == null) return array;
+  inOrderTraverse(tree.left, array);
+  array.add(tree.value);
+  inOrderTraverse(tree.right, array);
+  return array; // Unreachable
+}
+
+public static ArrayList<Integer> preOrderTraverse(BST tree, ArrayList<Integer> array) {
+  if(tree == null) return array;
+  array.add(tree.value);
+  preOrderTraverse(tree.left, array);
+  preOrderTraverse(tree.right, array);
+  return array; // Unreachable
+}
+
+public static ArrayList<Integer> postOrderTraverse(BST tree, ArrayList<Integer> array) {
+  if(tree == null) return array;
+  postOrderTraverse(tree.left, array);
+  postOrderTraverse(tree.right, array);
+  array.add(tree.value);
+  return array; // Unreachable
+}
