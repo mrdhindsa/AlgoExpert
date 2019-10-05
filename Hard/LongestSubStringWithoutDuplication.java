7846fe2883414cca0e9584253c8865bb89dad5d8
Manpreet Dhindsa <46641374+mrdhindsa@users.noreply.github.com>
2019-10-05 15:49:16 -0700

    Update LongestSubStringWithoutDuplication.java

--- Hard/LongestSubStringWithoutDuplication.java
+++ Hard/LongestSubStringWithoutDuplication.java
@@ -1,27 +1,29 @@
 import java.util.*; // O(n*n) time complexity and O(n) space complexity
 class Program {
+	
   public static String longestSubstringWithoutDuplication(String str) {
-		int max = Integer.MIN_VALUE, size = str.length();
-		String to_return = "";
-		ArrayList<Character> chars = new ArrayList<Character>();
-		for(int i = 0; i < size; i++){
-			String temp = "";
-			int k = i;
-			while(k < size){
-				char letter = str.charAt(k);
-				if(chars.contains(letter)){
-					break;
-				}
-				chars.add(letter);
-				temp += letter;
-				k++;
-			}
-			if(chars.size() > max){
-				max = chars.size();
-				to_return = temp;
-			}
-			chars.clear();
-		}
-		return to_return;
+    int max = Integer.MIN_VALUE, size = str.length();
+    String to_return = "";
+    ArrayList<Character> chars = new ArrayList<Character>();
+    for(int i = 0; i < size; i++){
+      String temp = "";
+      int k = i;
+      while(k < size){
+	char letter = str.charAt(k);
+	if(chars.contains(letter)){
+	  break;
+	}
+	chars.add(letter);
+	temp += letter;
+	k++;
+      }
+      if(chars.size() > max){
+	max = chars.size();
+	to_return = temp;
+      }
+      chars.clear();
+    }
+    return to_return;
   }
 }
+
