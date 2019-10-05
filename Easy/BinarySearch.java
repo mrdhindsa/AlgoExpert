317c71d7fe4ceb3d9dac08a3ab560d7afbe71342
Manpreet Dhindsa <46641374+mrdhindsa@users.noreply.github.com>
2019-10-05 15:30:28 -0700

    Create BinarySearch.java

---
+++ Easy/BinarySearch.java
@@ -0,0 +1,15 @@
+// O(logn) \\ Iterative binary search 
+public static int binarySearch(int[] array, int target) {
+  int left = 0, right = array.length - 1;
+  while(left <= right){
+    int middle = (left + right) / 2, value = array[middle];
+    if(value == target) return middle;
+    else if(value > target){
+      right = middle - 1;
+    }	
+    else{
+      left = middle + 1;
+    }
+  }
+  return -1;
+}
