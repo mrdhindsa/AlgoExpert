aa9940c56747a906680468f45a65b7eb63c6fb88
Manpreet Dhindsa <46641374+mrdhindsa@users.noreply.github.com>
2019-10-05 14:55:54 -0700

    Create RemoveKthNodefromLinkedList.java

---
+++ Medium/RemoveKthNodefromLinkedList.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+class Program {
+   static class LinkedList {
+    int value;
+    LinkedList next = null;
+
+    public LinkedList(int value) {
+      this.value = value;
+    }
+  }
+  
+  public static void removeKthNodeFromEnd(LinkedList head, int k) { // Using one pointer (traversing the list twice) // O(n) \\
+		LinkedList temp = head, new_temp;
+		int count = 0;
+		while(temp != null){
+			temp = temp.next;
+			count++;
+		}
+		if(count == k){ // Must remove head. Why doesnt head = head.next; work?
+			head.value = head.next.value; 
+		  head.next = head.next.next;
+			return;
+		}
+		temp = head;
+		for(int i = 0; i + 1 + k < count; i++){
+			temp = temp.next;
+		}
+		temp.next = temp.next.next;
+  }
+}
