0befa6c3794cf4772be9f28fb2fe8596a8303d69
Manpreet Dhindsa <46641374+mrdhindsa@users.noreply.github.com>
2019-10-05 11:19:33 -0700

    Update LongestPalindromicString.java

--- Medium/LongestPalindromicString.java
+++ Medium/LongestPalindromicString.java
@@ -12,20 +12,17 @@ class Brute_Implementation { // O(n*n*n) time complexity (required to create eac
   }
 	
   public static String longestPalindromicSubstring(String str){
-		ArrayList<String> sub_strings = new ArrayList<String>();
-		for(int i = 0; i < str.length(); i++){
-			for(int j = i+1; j <= str.length(); j++){
-				sub_strings.add(str.substring(i,j));
-			}
-		}
-		int max = Integer.MIN_VALUE;
-		String to_return = "";
-		for(String s : sub_strings){
-				if(isPalindrome(s) && max < s.length()){
-						max = s.length();
-						to_return = s;
-				}
-		}
-		return to_return;
+    String to_return = "";
+    int max = Integer.MIN_VALUE;
+    for(int i = 0; i < str.length(); i++){
+      for(int j = i+1; j <= str.length(); j++){
+	String s = str.substring(i,j);
+	if(isPalindrome(s) && max < s.length()){
+	  max = s.length();
+	  to_return = s;
+	}
+      }
+    }
+    return to_return;
   }
 }
