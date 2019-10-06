470d93280533714c5ba9fb2c63bfd03e87acbca9
Manpreet Dhindsa <46641374+mrdhindsa@users.noreply.github.com>
2019-10-06 13:40:05 -0700

    Create FindLoop_LinkedList.java

---
+++ Hard/FindLoop_LinkedList.java
@@ -0,0 +1,15 @@
+// Given a Single Linked List which only knows this.value and this.next, find the loop.
+public static LinkedList brute_findLoop(LinkedList head){ // Uses O(n) time and O(n) space
+  ArrayList<LinkedList> arr = new ArrayList<LinkedList>();
+  LinkedList temp = head;
+  while(temp.next != null){
+    if(arr.contains(temp)){
+      return temp;
+    }
+    arr.add(temp);
+    temp = temp.next;
+  }
+  return head; // Unreachable
+}
+
+// Without Using extra space. O(1) space complexity
