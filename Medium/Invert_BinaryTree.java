147a7fee8b491296b63a9455dc830d396d9be7c1
Manpreet Dhindsa <46641374+mrdhindsa@users.noreply.github.com>
2019-10-07 15:56:24 -0700

    Update Invert_BinaryTree.java

--- Medium/Invert_BinaryTree.java
+++ Medium/Invert_BinaryTree.java
@@ -22,4 +22,21 @@ class Program {
     invertBinaryTree(tree.left);
     invertBinaryTree(tree.right);
   }
+  
+  public static void iterative_invertBinaryTree(BinaryTree tree) { // Using breadth first search. 33 ms runtime. O(n) time, O(n) space
+    Stack<BinaryTree> stack = new Stack<BinaryTree>(){{
+			add(tree);
+		}};
+		while(stack.empty() == false){
+			BinaryTree curr = stack.pop(), left = curr.left;
+			curr.left = curr.right;
+			curr.right = left;
+			if(curr.left != null){
+				stack.push(curr.left);
+			}
+			if(curr.right != null){
+				stack.push(curr.right);
+			}
+		}
+  }
 }
