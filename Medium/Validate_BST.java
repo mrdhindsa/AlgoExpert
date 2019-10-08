cbbb156a1c07942821e6fc68163c6349709242c4
Manpreet Dhindsa <46641374+mrdhindsa@users.noreply.github.com>
2019-10-08 10:12:37 -0700

    Update Validate_BST.java

--- Medium/Validate_BST.java
+++ Medium/Validate_BST.java
@@ -10,20 +10,20 @@
   
   // Use an inorder traversal
   public static boolean validateBst(BST tree) {
-		ArrayList<Integer> array = new ArrayList<Integer>();
-		inorder(tree, array);
-		System.out.println(Arrays.toString(array.toArray()));
-		for(int i = 1; i < array.size(); i++){
-			if(array.get(i) < array.get(i-1)) return false;
-		}
-		return true;
+    ArrayList<Integer> array = new ArrayList<Integer>();   
+    inorder(tree, array);
+    System.out.println(Arrays.toString(array.toArray()));
+    for(int i = 1; i < array.size(); i++){
+      if(array.get(i) < array.get(i-1)) return false;
+    }
+    return true;
   }
-	
-	public static void inorder(BST tree, ArrayList<Integer> array){
-		if(tree == null) return;
-		inorder(tree.left, array);
-		array.add(tree.value);
-		inorder(tree.right, array);
-	}
-  
+
+  public static void inorder(BST tree, ArrayList<Integer> array){
+    if(tree == null) return;
+    inorder(tree.left, array);
+    array.add(tree.value);
+    inorder(tree.right, array);
+  }
+
   // No extra space implementation
