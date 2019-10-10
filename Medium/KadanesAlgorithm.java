b225c02da523364409f52af4d93cfa2ae07f7091
Manpreet Dhindsa <46641374+mrdhindsa@users.noreply.github.com>
2019-10-09 17:03:05 -0700

    Update KadanesAlgorithm.java

--- Medium/KadanesAlgorithm.java
+++ Medium/KadanesAlgorithm.java
@@ -1,10 +1,10 @@
 // Given an array find the max sum of adjacent numbers
   public static int kadanesAlgorithm(int[] array){ // O(n) time and space
     int temp = array[0];
-		int curr_max = array[0];
-		for(int i = 1; i < array.length; i++){
-			temp = Math.max(temp + array[i], array[i]);
-			curr_max = Math.max(curr_max, temp);
-		}
-		return curr_max;
+    int curr_max = array[0];
+    for(int i = 1; i < array.length; i++){
+      temp = Math.max(temp + array[i], array[i]);
+      curr_max = Math.max(curr_max, temp);
+    }
+    return curr_max;
   }
