751ccb5abd1a7dd6db0588bfc26c4d9c0367cda2
Manpreet Dhindsa <46641374+mrdhindsa@users.noreply.github.com>
2019-10-21 16:41:13 -0700

    Update getNthfib.java

--- Easy/getNthfib.java
+++ Easy/getNthfib.java
@@ -1,4 +1,4 @@
-//  Return the nth fibonnaci number
+// Return the nth fibonnaci number. 1 -> 0 , 2 -> 1 (count is shifted by 1)
 public static int getNthFib(int n){
   if(n == 1 || n == 2) return n - 1;
   int curr = 0, curr_1 = 1, curr_2 = 0;
