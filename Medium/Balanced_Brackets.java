e3e23f8bf5eb931bc36213fd70219bd6bbb98085
Manpreet Dhindsa <46641374+mrdhindsa@users.noreply.github.com>
2019-10-22 09:32:57 -0700

    Update Balanced_Brackets.java

--- Medium/Balanced_Brackets.java
+++ Medium/Balanced_Brackets.java
@@ -25,7 +25,6 @@ public static boolean balancedBrackets(String str) {
 
 // Another implementation Using HashMap
 public static boolean balancedBrackets(String str) {
-  // Write your code here.
   if(str.length() % 2 != 0) return false;
   HashMap<Character, Character> my_hash = new HashMap<Character, Character>()
     {{
@@ -34,8 +33,7 @@ public static boolean balancedBrackets(String str) {
       put(']','[');
     }};
   Stack<Character> stack = new Stack<Character>(); 
-  for(int i = 0; i < str.length(); i++){
-    char letter = str.charAt(i);
+  for(char letter : str.toCharArray()){
     if(letter == '[' || letter == '(' || letter == '{') stack.push(letter);
     else if(letter == ']' || letter == ')' || letter == '}'){
       if(stack.size() == 0) return false;
@@ -44,6 +42,5 @@ public static boolean balancedBrackets(String str) {
     }
     else{ return false; }
   }
-  if(stack.size() != 0) return false;
-  return true;
+  return stack.empty();
 }
