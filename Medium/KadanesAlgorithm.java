e0995d4417f1addca87705f931268129e7f420ad
Manpreet Dhindsa <46641374+mrdhindsa@users.noreply.github.com>
2019-11-09 14:01:26 -0800

    Update KadanesAlgorithm.java

--- Medium/KadanesAlgorithm.java
+++ Medium/KadanesAlgorithm.java
@@ -1,4 +1,8 @@
 // Given an array find the max sum of adjacent numbers
+
+// Use [3,-2,4] to think about it
+// temp is checking should I continue a count or reset it
+// curr_max is the actual max check
   public static int kadanesAlgorithm(int[] array){ // O(n) time and space
     int temp = array[0];
     int curr_max = array[0];
